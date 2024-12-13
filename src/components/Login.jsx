9861d8281a9ab0c8afa40b6a713987e341e35ae6
KI-Anik <iamkhairul101@gmail.com>
2024-12-13 20:33:48 +0600

    pages design done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin=(e)=>{
+        e.preventDefault()
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+    }
   return (
-    <div>Login</div>
+    <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+    </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <form onSubmit={handleLogin} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input name='email' type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input name='password' type="password" placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      <p>new here? please <Link to={'/register'} className='text-blue-500'>Register</Link> first</p>
+      </form>
+    </div>
+  </div>
+</div>
   )
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const handleRegister=(e)=>{
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password)
+    }
+
   return (
-    <div>Register</div>
+    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="text-center lg:text-left">
+        <h1 className="text-5xl font-bold">Register now!</h1>
+      </div>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form onSubmit={handleRegister} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input name='name' type="text" placeholder="name" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input name='email' type="email" placeholder="email" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input name='password' type="password" placeholder="password" className="input input-bordered" required />
+           
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Register</button>
+          </div>
+        <p>Already have an account? then <Link to={'/login'} className='text-blue-500'>Login</Link></p>
+        </form>
+      </div>
+    </div>
+  </div>
   )
 }
 
